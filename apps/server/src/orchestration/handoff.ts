Alegherix/Luminor

Optimize native session forking across providers (#628)
* Optimize native session forking across providers

- Reuse shared ACP fork handling and lifecycle-safe cursor registration
- Add Claude native fork support with validation and coverage
- Increase bootstrap transcript budgets and centralize input composition

* Fix native fork recovery

apps/server/src/orchestration/handoff.ts
@@ -1,13 +1,8 @@
-import {
-  PROVIDER_SEND_TURN_MAX_INPUT_CHARS,
-  type OrchestrationMessage,
-  type OrchestrationThread,
-} from "@synara/contracts";
+import type { OrchestrationMessage, OrchestrationThread } from "@synara/contracts";
 
 const RECENT_MESSAGE_COUNT = 6;
 const EARLIER_MESSAGE_CHAR_LIMIT = 320;
 const RECENT_MESSAGE_CHAR_LIMIT = 2_400;
-const HANDOFF_BOOTSTRAP_CHAR_BUDGET = Math.floor(PROVIDER_SEND_TURN_MAX_INPUT_CHARS * 0.75);
 // Hard ceiling for any bootstrap transcript: it replays as one uncached user
 // message, so long threads must drop their oldest summaries rather than grow.
 const BOOTSTRAP_TRANSCRIPT_CHAR_BUDGET = 32_000;
@@ -180,7 +175,7 @@ function buildImportedMessagesBootstrapText(input: {
 
 export function buildHandoffBootstrapText(
   thread: Pick<OrchestrationThread, "title" | "branch" | "worktreePath" | "handoff" | "messages">,
-  maxChars = HANDOFF_BOOTSTRAP_CHAR_BUDGET,
+  maxChars = BOOTSTRAP_TRANSCRIPT_CHAR_BUDGET,
 ): string | null {
   const importedMessages = listImportedHandoffMessages(thread);
   if (importedMessages.length === 0 || thread.handoff === null) {
@@ -198,7 +193,7 @@ export function buildHandoffBootstrapText(
 export function buildPriorTranscriptBootstrapText(
   thread: Pick<OrchestrationThread, "title" | "branch" | "worktreePath" | "messages">,
   currentMessageId: string,
-  maxChars = HANDOFF_BOOTSTRAP_CHAR_BUDGET,
+  maxChars = BOOTSTRAP_TRANSCRIPT_CHAR_BUDGET,
 ): string | null {
   const priorMessages = listPriorTranscriptMessages(thread, currentMessageId);
   if (priorMessages.length === 0) {
@@ -216,7 +211,7 @@ export function buildPriorTranscriptBootstrapText(
 
 export function buildForkBootstrapText(
   thread: Pick<OrchestrationThread, "title" | "branch" | "worktreePath" | "messages">,
-  maxChars = HANDOFF_BOOTSTRAP_CHAR_BUDGET,
+  maxChars = BOOTSTRAP_TRANSCRIPT_CHAR_BUDGET,
 ): string | null {
   const importedMessages = listImportedForkMessages(thread);
   if (importedMessages.length === 0) {

apps/server/src/provider/Layers/DroidAdapter.ts
@@ -87,6 +87,7 @@ import {
   settleAcpPendingUserInputsAsEmptyAnswers,
   withAcpPlanModePrompt,
 } from "../acp/AcpAdapterSessionSupport.ts";
+import { forkViaAcpRuntime } from "../acp/acpFork.ts";
 import { type AcpSessionRuntimeShape } from "../acp/AcpSessionRuntime.ts";
 import {
   makeAcpAssistantItemEvent,
@@ -1945,27 +1946,27 @@ export function makeDroidAdapter(
         }
 
         const forkRuntime = (runtime: AcpSessionRuntimeShape) =>
-          Effect.gen(function* () {
-            if (!(yield* runtime.supportsSessionFork)) {
-              return yield* new ProviderAdapterValidationError({
-                provider: PROVIDER,
-                operation: "forkThread",
-                issue:
-                  "This Droid ACP version does not advertise session/fork; Synara will rebuild the fork from its retained transcript.",
-              });
-            }
-            return yield* runtime.forkSession({ cwd: targetCwd, mcpServers: [] });
-          }).pipe(
-            Effect.timeoutOption(DROID_ACP_REQUEST_TIMEOUT_MS),
-            Effect.flatMap(
-              Option.match({
-                onNone: () => Effect.fail(droidAcpTimeoutError("session/fork")),
-                onSome: Effect.succeed,
-              }),
-            ),
-          );
+          forkViaAcpRuntime({
+            provider: PROVIDER,
+            runtime,
+            targetCwd,
+            unsupportedIssue:
+              "This Droid ACP version does not advertise session/fork; Synara will rebuild the fork from its retained transcript.",
+            requestTimeoutMs: DROID_ACP_REQUEST_TIMEOUT_MS,
+            timeoutError: droidAcpTimeoutError,
+          });
 
         const activeSource = sessions.get(input.sourceThreadId);
+        // Forking mid-turn would branch from incomplete in-flight state, so
+        // let the retained-transcript fallback handle busy sources.
+        if (activeSource?.activeTurnId !== undefined) {
+          return yield* new ProviderAdapterValidationError({
+            provider: PROVIDER,
+            operation: "forkThread",
+            issue:
+              "The source Droid session has a turn in flight; Synara will rebuild the fork from its retained transcript.",
+          });
+        }
         const forked = activeSource
           ? yield* forkRuntime(activeSource.acp)
           : yield* Effect.gen(function* () {
@@ -2001,20 +2002,17 @@ export function makeDroidAdapter(
               return yield* forkRuntime(runtime);
             }).pipe(Effect.scoped);
 
-        const resumeCursor = {
-          schemaVersion: DROID_RESUME_VERSION,
-          sessionId: forked.sessionId,
-        };
-        yield* startSession({
+        // Return only the cursor: ProviderService registers the binding under
+        // a committed lifecycle lease and the target's first turn resumes it
+        // there. Starting the runtime here would capture an undefined
+        // lifecycle generation, orphaning the fork's approval requests.
+        return {
           threadId: input.threadId,
-          provider: PROVIDER,
-          cwd: targetCwd,
-          runtimeMode: input.runtimeMode,
-          resumeCursor,
-          ...(input.modelSelection ? { modelSelection: input.modelSelection } : {}),
-          ...(input.providerOptions ? { providerOptions: input.providerOptions } : {}),
-        });
-        return { threadId: input.threadId, resumeCursor };
+          resumeCursor: {
+            schemaVersion: DROID_RESUME_VERSION,
+            sessionId: forked.sessionId,
+          },
+        };
       }).pipe(
         Effect.mapError((cause) =>
           cause instanceof ProviderAdapterRequestError ||

apps/server/src/provider/Layers/OpenCodeAdapter.ts
@@ -4365,6 +4365,15 @@ export function makeOpenCodeAdapterLive(options?: OpenCodeAdapterLiveOptions) {
       const forkThread: NonNullable<OpenCodeAdapterShape["forkThread"]> = (input) =>
         Effect.gen(function* () {
           const sourceContext = sessions.get(input.sourceThreadId);
+          // Forking mid-turn would branch from incomplete in-flight state, so
+          // let the retained-transcript fallback handle busy sources.
+          if (sourceContext?.activeTurnId !== undefined) {
+            return yield* new ProviderAdapterValidationError({
+              provider,
+              operation: "forkThread",
+              issue: `The source ${adapterConfig.displayName} session has a turn in flight; Synara will rebuild the fork from its retained transcript.`,
+            });
+          }
           const sourceSessionId =
             sourceContext?.openCodeSessionId ?? extractResumeSessionId(input.sourceResumeCursor);
           if (!sourceSessionId) {
@@ -4434,19 +4443,13 @@ export function makeOpenCodeAdapterLive(options?: OpenCodeAdapterLiveOptions) {
             });
           }
 
-          const session = yield* startSession({
-            threadId: input.threadId,
-            provider,
-            cwd: targetDirectory,
-            ...(input.modelSelection ? { modelSelection: input.modelSelection } : {}),
-            ...(input.providerOptions ? { providerOptions: input.providerOptions } : {}),
-            resumeCursor: { openCodeSessionId: forkedSessionId, cwd: targetDirectory },
-            runtimeMode: input.runtimeMode,
-          });
-
+          // Return only the cursor: ProviderService registers the binding under
+          // a committed lifecycle lease and the target's first turn resumes it
+          // there. Starting the runtime here would capture an undefined
+          // lifecycle generation, orphaning the fork's approval requests.
           return {
             threadId: input.threadId,
-            ...(session.resumeCursor !== undefined ? { resumeCursor: session.resumeCursor } : {}),
+            resumeCursor: { openCodeSessionId: forkedSessionId, cwd: targetDirectory },
           };
         });
 

apps/server/src/provider/acp/AcpJsonRpcConnection.test.ts
@@ -17,6 +17,8 @@ import {
   type AcpProtocolLogEvent,
   type AcpSessionRequestLogEvent,
 } from "./AcpSessionRuntime.ts";
+import { forkViaAcpRuntime } from "./acpFork.ts";
+import { ProviderAdapterRequestError, ProviderAdapterValidationError } from "../Errors.ts";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const mockAgentPath = path.join(__dirname, "../../../scripts/acp-mock-agent.ts");
@@ -336,6 +338,57 @@ describe("AcpSessionRuntime", () => {
           },
           cwd: process.cwd(),
           clientCapabilities: { _meta: { parameterizedModelPicker: true } },
+          clientInfo: { name: "synara-test", version: "0.0.0" },
+          authMethodId: "test",
+          requestLogger: (event) =>
+            Effect.sync(() => {
+              requestEvents.push(event);
+            }),
+        }),
+      ),
+      Effect.scoped,
+      Effect.provide(NodeServices.layer),
+    );
+  });
+
+  it.effect("rejects fork cursors when the ACP agent cannot reopen sessions", () => {
+    const requestEvents: Array<AcpSessionRequestLogEvent> = [];
+    return Effect.gen(function* () {
+      const runtime = yield* AcpSessionRuntime;
+      yield* runtime.start();
+      expect(yield* runtime.supportsSessionFork).toBe(true);
+      expect(yield* runtime.supportsSessionRecovery).toBe(false);
+
+      const error = yield* forkViaAcpRuntime({
+        provider: "test",
+        runtime,
+        targetCwd: process.cwd(),
+        unsupportedIssue: "fork unsupported",
+        requestTimeoutMs: 1_000,
+        timeoutError: (method) =>
+          new ProviderAdapterRequestError({
+            provider: "test",
+            method,
+            detail: "timed out",
+          }),
+      }).pipe(Effect.flip);
+
+      expect(error).toBeInstanceOf(ProviderAdapterValidationError);
+      expect(error.message).toContain("cannot reopen the forked session");
+      expect(requestEvents.some((event) => event.method === "session/fork")).toBe(false);
+    }).pipe(
+      Effect.provide(
+        AcpSessionRuntime.layer({
+          spawn: {
+            command: bunExe,
+            args: [mockAgentPath],
+            env: {
+              VITEST: "true",
+              SYNARA_ACP_SUPPORT_SESSION_FORK: "1",
+              SYNARA_ACP_SUPPORT_SESSION_LOAD: "0",
+            },
+          },
+          cwd: process.cwd(),
           clientInfo: { name: "synara-test", version: "0.0.0" },
           authMethodId: "test",
           requestLogger: (event) =>

apps/server/src/provider/acp/acpFork.ts
@@ -1,0 +1,55 @@
+// FILE: acpFork.ts
+// Purpose: Shared "probe capability + session/fork" step for ACP-backed provider adapters.
+// Layer: Provider ACP helper
+// Exports: forkViaAcpRuntime
+
+import type * as Acp from "@agentclientprotocol/sdk";
+import { Effect, Option } from "effect";
+import type * as AcpErrors from "./AcpErrors.ts";
+import type { AcpSessionRuntimeShape } from "./AcpSessionRuntime.ts";
+import { ProviderAdapterRequestError, ProviderAdapterValidationError } from "../Errors.ts";
+
+/**
+ * Fork the runtime's active session when the agent advertises `session/fork`.
+ *
+ * Fails with a `ProviderAdapterValidationError` when the capability is missing
+ * so callers fall back to Synara's retained-transcript fork, and bounds the
+ * whole probe+fork exchange with the adapter's request timeout.
+ */
+export function forkViaAcpRuntime(input: {
+  readonly provider: string;
+  readonly runtime: AcpSessionRuntimeShape;
+  readonly targetCwd: string;
+  readonly unsupportedIssue: string;
+  readonly requestTimeoutMs: number;
+  readonly timeoutError: (method: string) => ProviderAdapterRequestError;
+}): Effect.Effect<
+  Acp.ForkSessionResponse,
+  AcpErrors.AcpError | ProviderAdapterRequestError | ProviderAdapterValidationError
+> {
+  return Effect.gen(function* () {
+    if (!(yield* input.runtime.supportsSessionFork)) {
+      return yield* new ProviderAdapterValidationError({
+        provider: input.provider,
+        operation: "forkThread",
+        issue: input.unsupportedIssue,
+      });
+    }
+    if (!(yield* input.runtime.supportsSessionRecovery)) {
+      return yield* new ProviderAdapterValidationError({
+        provider: input.provider,
+        operation: "forkThread",
+        issue: `This ${input.provider} ACP version advertises session/fork but cannot reopen the forked session; Synara will rebuild the fork from its retained transcript.`,
+      });
+    }
+    return yield* input.runtime.forkSession({ cwd: input.targetCwd, mcpServers: [] });
+  }).pipe(
+    Effect.timeoutOption(input.requestTimeoutMs),
+    Effect.flatMap(
+      Option.match({
+        onNone: () => Effect.fail(input.timeoutError("session/fork")),
+        onSome: Effect.succeed,
+      }),
+    ),
+  );
+}

piai-lab/Haros

apps/server/src/orchestration/handoff.ts
@@ -1,13 +1,8 @@
-import {
-  PROVIDER_SEND_TURN_MAX_INPUT_CHARS,
-  type OrchestrationMessage,
-  type OrchestrationThread,
-} from "@omnimind/contracts";
+import type { OrchestrationMessage, OrchestrationThread } from "@omnimind/contracts";
 
 const RECENT_MESSAGE_COUNT = 6;
 const EARLIER_MESSAGE_CHAR_LIMIT = 320;
 const RECENT_MESSAGE_CHAR_LIMIT = 2_400;
-const HANDOFF_BOOTSTRAP_CHAR_BUDGET = Math.floor(PROVIDER_SEND_TURN_MAX_INPUT_CHARS * 0.75);
 // Hard ceiling for any bootstrap transcript: it replays as one uncached user
 // message, so long threads must drop their oldest summaries rather than grow.
 const BOOTSTRAP_TRANSCRIPT_CHAR_BUDGET = 32_000;
@@ -180,7 +175,7 @@ function buildImportedMessagesBootstrapText(input: {
 
 export function buildHandoffBootstrapText(
   thread: Pick<OrchestrationThread, "title" | "branch" | "worktreePath" | "handoff" | "messages">,
-  maxChars = HANDOFF_BOOTSTRAP_CHAR_BUDGET,
+  maxChars = BOOTSTRAP_TRANSCRIPT_CHAR_BUDGET,
 ): string | null {
   const importedMessages = listImportedHandoffMessages(thread);
   if (importedMessages.length === 0 || thread.handoff === null) {
@@ -198,7 +193,7 @@ export function buildHandoffBootstrapText(
 export function buildPriorTranscriptBootstrapText(
   thread: Pick<OrchestrationThread, "title" | "branch" | "worktreePath" | "messages">,
   currentMessageId: string,
-  maxChars = HANDOFF_BOOTSTRAP_CHAR_BUDGET,
+  maxChars = BOOTSTRAP_TRANSCRIPT_CHAR_BUDGET,
 ): string | null {
   const priorMessages = listPriorTranscriptMessages(thread, currentMessageId);
   if (priorMessages.length === 0) {
@@ -216,7 +211,7 @@ export function buildPriorTranscriptBootstrapText(
 
 export function buildForkBootstrapText(
   thread: Pick<OrchestrationThread, "title" | "branch" | "worktreePath" | "messages">,
-  maxChars = HANDOFF_BOOTSTRAP_CHAR_BUDGET,
+  maxChars = BOOTSTRAP_TRANSCRIPT_CHAR_BUDGET,
 ): string | null {
   const importedMessages = listImportedForkMessages(thread);
   if (importedMessages.length === 0) {

apps/server/src/provider/Layers/DroidAdapter.ts
@@ -87,6 +87,7 @@ import {
   settleAcpPendingUserInputsAsEmptyAnswers,
   withAcpPlanModePrompt,
 } from "../acp/AcpAdapterSessionSupport.ts";
+import { forkViaAcpRuntime } from "../acp/acpFork.ts";
 import { type AcpSessionRuntimeShape } from "../acp/AcpSessionRuntime.ts";
 import {
   makeAcpAssistantItemEvent,
@@ -1945,27 +1946,27 @@ export function makeDroidAdapter(
         }
 
         const forkRuntime = (runtime: AcpSessionRuntimeShape) =>
-          Effect.gen(function* () {
-            if (!(yield* runtime.supportsSessionFork)) {
-              return yield* new ProviderAdapterValidationError({
-                provider: PROVIDER,
-                operation: "forkThread",
-                issue:
-                  "This Droid ACP version does not advertise session/fork; OmniMind will rebuild the fork from its retained transcript.",
-              });
-            }
-            return yield* runtime.forkSession({ cwd: targetCwd, mcpServers: [] });
-          }).pipe(
-            Effect.timeoutOption(DROID_ACP_REQUEST_TIMEOUT_MS),
-            Effect.flatMap(
-              Option.match({
-                onNone: () => Effect.fail(droidAcpTimeoutError("session/fork")),
-                onSome: Effect.succeed,
-              }),
-            ),
-          );
+          forkViaAcpRuntime({
+            provider: PROVIDER,
+            runtime,
+            targetCwd,
+            unsupportedIssue:
+              "This Droid ACP version does not advertise session/fork; OmniMind will rebuild the fork from its retained transcript.",
+            requestTimeoutMs: DROID_ACP_REQUEST_TIMEOUT_MS,
+            timeoutError: droidAcpTimeoutError,
+          });
 
         const activeSource = sessions.get(input.sourceThreadId);
+        // Forking mid-turn would branch from incomplete in-flight state, so
+        // let the retained-transcript fallback handle busy sources.
+        if (activeSource?.activeTurnId !== undefined) {
+          return yield* new ProviderAdapterValidationError({
+            provider: PROVIDER,
+            operation: "forkThread",
+            issue:
+              "The source Droid session has a turn in flight; OmniMind will rebuild the fork from its retained transcript.",
+          });
+        }
         const forked = activeSource
           ? yield* forkRuntime(activeSource.acp)
           : yield* Effect.gen(function* () {
@@ -2001,20 +2002,17 @@ export function makeDroidAdapter(
               return yield* forkRuntime(runtime);
             }).pipe(Effect.scoped);
 
-        const resumeCursor = {
-          schemaVersion: DROID_RESUME_VERSION,
-          sessionId: forked.sessionId,
-        };
-        yield* startSession({
+        // Return only the cursor: ProviderService registers the binding under
+        // a committed lifecycle lease and the target's first turn resumes it
+        // there. Starting the runtime here would capture an undefined
+        // lifecycle generation, orphaning the fork's approval requests.
+        return {
           threadId: input.threadId,
-          provider: PROVIDER,
-          cwd: targetCwd,
-          runtimeMode: input.runtimeMode,
-          resumeCursor,
-          ...(input.modelSelection ? { modelSelection: input.modelSelection } : {}),
-          ...(input.providerOptions ? { providerOptions: input.providerOptions } : {}),
-        });
-        return { threadId: input.threadId, resumeCursor };
+          resumeCursor: {
+            schemaVersion: DROID_RESUME_VERSION,
+            sessionId: forked.sessionId,
+          },
+        };
       }).pipe(
         Effect.mapError((cause) =>
           cause instanceof ProviderAdapterRequestError ||

apps/server/src/provider/Layers/OpenCodeAdapter.ts
@@ -4368,6 +4368,15 @@ export function makeOpenCodeAdapterLive(options?: OpenCodeAdapterLiveOptions) {
       const forkThread: NonNullable<OpenCodeAdapterShape["forkThread"]> = (input) =>
         Effect.gen(function* () {
           const sourceContext = sessions.get(input.sourceThreadId);
+          // Forking mid-turn would branch from incomplete in-flight state, so
+          // let the retained-transcript fallback handle busy sources.
+          if (sourceContext?.activeTurnId !== undefined) {
+            return yield* new ProviderAdapterValidationError({
+              provider,
+              operation: "forkThread",
+              issue: `The source ${adapterConfig.displayName} session has a turn in flight; OmniMind will rebuild the fork from its retained transcript.`,
+            });
+          }
           const sourceSessionId =
             sourceContext?.openCodeSessionId ?? extractResumeSessionId(input.sourceResumeCursor);
           if (!sourceSessionId) {
@@ -4437,19 +4446,13 @@ export function makeOpenCodeAdapterLive(options?: OpenCodeAdapterLiveOptions) {
             });
           }
 
-          const session = yield* startSession({
-            threadId: input.threadId,
-            provider,
-            cwd: targetDirectory,
-            ...(input.modelSelection ? { modelSelection: input.modelSelection } : {}),
-            ...(input.providerOptions ? { providerOptions: input.providerOptions } : {}),
-            resumeCursor: { openCodeSessionId: forkedSessionId, cwd: targetDirectory },
-            runtimeMode: input.runtimeMode,
-          });
-
+          // Return only the cursor: ProviderService registers the binding under
+          // a committed lifecycle lease and the target's first turn resumes it
+          // there. Starting the runtime here would capture an undefined
+          // lifecycle generation, orphaning the fork's approval requests.
           return {
             threadId: input.threadId,
-            ...(session.resumeCursor !== undefined ? { resumeCursor: session.resumeCursor } : {}),
+            resumeCursor: { openCodeSessionId: forkedSessionId, cwd: targetDirectory },
           };
         });
 

apps/server/src/provider/acp/AcpJsonRpcConnection.test.ts
@@ -17,6 +17,8 @@ import {
   type AcpProtocolLogEvent,
   type AcpSessionRequestLogEvent,
 } from "./AcpSessionRuntime.ts";
+import { forkViaAcpRuntime } from "./acpFork.ts";
+import { ProviderAdapterRequestError, ProviderAdapterValidationError } from "../Errors.ts";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const mockAgentPath = path.join(__dirname, "../../../scripts/acp-mock-agent.ts");
@@ -336,6 +338,57 @@ describe("AcpSessionRuntime", () => {
           },
           cwd: process.cwd(),
           clientCapabilities: { _meta: { parameterizedModelPicker: true } },
+          clientInfo: { name: "omnimind-test", version: "0.0.0" },
+          authMethodId: "test",
+          requestLogger: (event) =>
+            Effect.sync(() => {
+              requestEvents.push(event);
+            }),
+        }),
+      ),
+      Effect.scoped,
+      Effect.provide(NodeServices.layer),
+    );
+  });
+
+  it.effect("rejects fork cursors when the ACP agent cannot reopen sessions", () => {
+    const requestEvents: Array<AcpSessionRequestLogEvent> = [];
+    return Effect.gen(function* () {
+      const runtime = yield* AcpSessionRuntime;
+      yield* runtime.start();
+      expect(yield* runtime.supportsSessionFork).toBe(true);
+      expect(yield* runtime.supportsSessionRecovery).toBe(false);
+
+      const error = yield* forkViaAcpRuntime({
+        provider: "test",
+        runtime,
+        targetCwd: process.cwd(),
+        unsupportedIssue: "fork unsupported",
+        requestTimeoutMs: 1_000,
+        timeoutError: (method) =>
+          new ProviderAdapterRequestError({
+            provider: "test",
+            method,
+            detail: "timed out",
+          }),
+      }).pipe(Effect.flip);
+
+      expect(error).toBeInstanceOf(ProviderAdapterValidationError);
+      expect(error.message).toContain("cannot reopen the forked session");
+      expect(requestEvents.some((event) => event.method === "session/fork")).toBe(false);
+    }).pipe(
+      Effect.provide(
+        AcpSessionRuntime.layer({
+          spawn: {
+            command: bunExe,
+            args: [mockAgentPath],
+            env: {
+              VITEST: "true",
+              SYNARA_ACP_SUPPORT_SESSION_FORK: "1",
+              SYNARA_ACP_SUPPORT_SESSION_LOAD: "0",
+            },
+          },
+          cwd: process.cwd(),
           clientInfo: { name: "omnimind-test", version: "0.0.0" },
           authMethodId: "test",
           requestLogger: (event) =>

apps/server/src/provider/acp/acpFork.ts
@@ -1,0 +1,55 @@
+// FILE: acpFork.ts
+// Purpose: Shared "probe capability + session/fork" step for ACP-backed provider adapters.
+// Layer: Provider ACP helper
+// Exports: forkViaAcpRuntime
+
+import type * as Acp from "@agentclientprotocol/sdk";
+import { Effect, Option } from "effect";
+import type * as AcpErrors from "./AcpErrors.ts";
+import type { AcpSessionRuntimeShape } from "./AcpSessionRuntime.ts";
+import { ProviderAdapterRequestError, ProviderAdapterValidationError } from "../Errors.ts";
+
+/**
+ * Fork the runtime's active session when the agent advertises `session/fork`.
+ *
+ * Fails with a `ProviderAdapterValidationError` when the capability is missing
+ * so callers fall back to OmniMind's retained-transcript fork, and bounds the
+ * whole probe+fork exchange with the adapter's request timeout.
+ */
+export function forkViaAcpRuntime(input: {
+  readonly provider: string;
+  readonly runtime: AcpSessionRuntimeShape;
+  readonly targetCwd: string;
+  readonly unsupportedIssue: string;
+  readonly requestTimeoutMs: number;
+  readonly timeoutError: (method: string) => ProviderAdapterRequestError;
+}): Effect.Effect<
+  Acp.ForkSessionResponse,
+  AcpErrors.AcpError | ProviderAdapterRequestError | ProviderAdapterValidationError
+> {
+  return Effect.gen(function* () {
+    if (!(yield* input.runtime.supportsSessionFork)) {
+      return yield* new ProviderAdapterValidationError({
+        provider: input.provider,
+        operation: "forkThread",
+        issue: input.unsupportedIssue,
+      });
+    }
+    if (!(yield* input.runtime.supportsSessionRecovery)) {
+      return yield* new ProviderAdapterValidationError({
+        provider: input.provider,
+        operation: "forkThread",
+        issue: `This ${input.provider} ACP version advertises session/fork but cannot reopen the forked session; OmniMind will rebuild the fork from its retained transcript.`,
+      });
+    }
+    return yield* input.runtime.forkSession({ cwd: input.targetCwd, mcpServers: [] });
+  }).pipe(
+    Effect.timeoutOption(input.requestTimeoutMs),
+    Effect.flatMap(
+      Option.match({
+        onNone: () => Effect.fail(input.timeoutError("session/fork")),
+        onSome: Effect.succeed,
+      }),
+    ),
+  );
+}