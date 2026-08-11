Alegherix/Luminor

feat(device): iOS Simulator pane (#529)
* feat(contracts): add device pane schemas and frame envelope

* feat(web): add device pane logic core and thread state store

Pure decision logic for the iOS Simulator pane: the H.264 frame gate
(codec-config -> keyframe -> streaming, with sequence-gap resync), canvas
to device-point mapping through the letterboxed contain rect, tap/swipe
classification, Simulator.app hardware chords, HID key translation, and
the picker/availability view models.

The state store mirrors browserStateStore's version gating so a delayed
RPC response cannot roll thread device state back behind a newer push. It
is intentionally not persisted: boot state only means something while the
reporting server is alive.

* feat(server): add device backend, manager, and frame transport

Introduces the macOS-only device engine behind the iOS simulator pane:

- DeviceBackend interface plus a deterministic FakeDeviceBackend for tests
  and an IosSimulatorBackend that discovers and drives simulators through
  simctl, delegating input, accessibility, and video to the native helper.
- helperClient isolates the helper's JSON-RPC-over-stdio and frame-socket
  protocol in one file so it can be adapted when the helper lands.
- DeviceManager owns thread-scoped attachment, versioned ThreadDeviceState
  snapshots, boot-source tracking, the Synara boot cap, and the shutdown
  rules (quit, thread archive, idle-after-detach; never user-booted).
- DeviceFrameTransport fans encoded frames out to WebSocket clients with
  bounded per-client queues and keyframe-aligned drops, so video can never
  starve RPC traffic on the shared socket.

* feat(server): expose device control to agents over the MCP gateway

Adds the device_* tool surface to the existing per-session agent gateway
rather than standing up a second MCP server: eleven ToolEntry handlers
gated on a new device:control capability, granted alongside browser
control at session issue and enforced by the existing mcpTranspor

apps/server/src/agentGateway/harnessPolicy.test.ts
@@ -94,4 +94,79 @@ describe("Synara harness policy", () => {
       assert.notInclude(text, "one exact synara_create_threads plan", provider);
     }
   });
+
+  it("teaches the device tools well enough for a plain prompt to work", () => {
+    const policy = renderSynaraHarnessPolicy({ gatewayControlAvailable: true });
+
+    // When to reach for them at all: the demo needed "using your device_* tools"
+    // spelled out because the policy only triggered on the user naming a tool.
+    assert.include(policy, "run, test, check, demo, debug, or interact with an iOS app");
+    assert.include(policy, "whether or not the user names a tool");
+    assert.include(policy, "never drive the simulator with xcrun simctl");
+    // A rival agent-device skill on the host was read before the tools were
+    // tried; the browser guidance names its competitors, so this does too.
+    assert.include(policy, "do not load or use an agent-device");
+    assert.include(policy, "rather than reading skill files first");
+
+    // The workflow, so the agent does not have to guess an ordering.
+    assert.include(policy, "device_list first");
+    // Reusing a booted device rather than starting a second one: two live
+    // simulators compete for the pane and the user watches the wrong screen.
+    assert.include(policy, "already booted, use that one");
+    assert.include(policy, "device_install and device_launch");
+    assert.include(policy, "com.apple.Preferences");
+
+    // Expo/RN CLI paths boot the sim through Simulator.app, which foregrounds
+    // a window the user is not watching and leaves the Synara pane empty. A
+    // real demo also stalled for minutes on a dev server holding the shell.
+    assert.include(policy, "For Expo or React Native work");
+    assert.include(policy, "expo start --ios");
+    assert.include(policy, "npm run ios");
+    assert.include(policy, "opens Simulator.app");
+    assert.include(policy, "exp://127.0.0.1:8081");
+    assert.include(policy, "start it detached in the background");
+
+    // Interaction discipline: describe before tapping, verify after.
+    assert.include(policy, "device points from device_describe_ui, never screenshot pixels");
+    assert.include(policy, "again afterwards to confirm the screen changed");
+
+    // Semantic targeting is the headline: making the model do the coordinate
+    // arithmetic is where taps go wrong, so label targeting leads.
+    assert.include(policy, "Tap by label rather than by coordinates");
+    assert.include(policy, "device_tap {udid, label}");
+    assert.include(policy, "device_tap {udid, label, role}");
+    assert.include(policy, "only when nothing in the tree labels the target");
+
+    // Why a row-centre tap does nothing, for the cases still using coordinates.
+    assert.include(policy, "the row's frame centre is dead space");
+
+    // Scrolling is motor control the server owns. A demo agent swiped three
+    // times to reach Developer when one call should have done it.
+    assert.include(policy, "Never write a swipe loop");
+    assert.include(policy, "device_scroll_to_element {udid, label}");
+    assert.include(policy, "device_tap with a label already scrolls");
+    // device_swipe still has a job; this must not read as a blanket ban.
+    assert.include(policy, "gestures that are the point in themselves");
+
+    // Reading and verifying toggle state from the tree instead of pixels. The
+    // same run took a screenshot purely to work out whether the switch moved.
+    assert.include(policy, "A toggle reports its state in the node's value");
+    assert.include(policy, "Never take a screenshot to check state");
+    assert.include(policy, "device_screenshot is for showing the user a result");
+
+    // The traps that made the demo agent report success it never observed.
+    assert.include(policy, "an unchanged tree after a tap means the tap missed");
+    assert.include(policy, "not delivered to the simulator");
+    assert.include(policy, "Never report success you have not observed");
+    assert.include(policy, "device_open_url always requires explicit user approval");
+    assert.include(policy, "Airplane Mode");
+  });
+
+  it("withholds device guidance from sessions with no gateway control", () => {
+    const policy = renderSynaraHarnessPolicy({ gatewayControlAvailable: false });
+
+    // Promising tools this session cannot reach would be a lie.
+    assert.notInclude(policy, "device_list");
+    assert.notInclude(policy, "device_describe_ui");
+  });
 });

apps/server/src/device/Services/DeviceService.ts
@@ -1,0 +1,18 @@
+import { ServiceMap } from "effect";
+
+import type { DeviceManager } from "../DeviceManager.ts";
+
+export interface DeviceServiceShape {
+  /**
+   * True only where a device backend can actually exist (macOS today). Off
+   * darwin the manager still answers, but every call reports
+   * `unsupported-platform`, and callers use this to hide the surface entirely
+   * rather than offering an agent eleven tools that cannot work.
+   */
+  readonly supported: boolean;
+  readonly manager: DeviceManager;
+}
+
+export class DeviceService extends ServiceMap.Service<DeviceService, DeviceServiceShape>()(
+  "synara/device/Services/DeviceService",
+) {}

apps/server/src/device/deviceFrameRoute.ts
@@ -1,0 +1,159 @@
+/**
+ * Device frame WebSocket route.
+ *
+ * Encoded video gets its own upgrade path rather than sharing the JSON RPC
+ * socket. Two reasons, both about not hurting the rest of the app:
+ *
+ * - Send-queue isolation. On one socket a video backlog sits ahead of RPC
+ *   responses in the same queue; on its own connection it cannot.
+ * - Compression. The RPC path negotiates per-message deflate; H.264 is already
+ *   compressed, so running it through zlib would burn CPU for nothing. This
+ *   path lands on the uncompressed WebSocket server (see
+ *   `upgradePathAllowsCompression`).
+ *
+ * Backpressure lives in `DeviceFrameTransport`; this module only adapts an
+ * Effect `Socket` into the transport's sink, tracking in-flight bytes itself
+ * because the Effect socket exposes no `bufferedAmount`.
+ *
+ * @module device/deviceFrameRoute
+ */
+import {
+  DEVICE_FRAME_RESYNC_MESSAGE,
+  DEVICE_FRAME_WS_PATH,
+  DEVICE_FRAME_WS_UDID_PARAM,
+} from "@synara/shared/deviceFrame";
+import { Effect, Layer } from "effect";
+import { HttpRouter, HttpServerRequest, HttpServerResponse } from "effect/unstable/http";
+
+import { DeviceService } from "./Services/DeviceService.ts";
+import type { DeviceFrameSink } from "./deviceFrameTransport.ts";
+
+/** A resync request is a few dozen bytes; anything larger is not one. */
+const MAX_CLIENT_MESSAGE_BYTES = 1_024;
+
+/**
+ * Parse a client message on the frame socket. Returns the request kind, or
+ * null for anything unrecognized, which the caller ignores rather than
+ * treating as a protocol error.
+ */
+export function decodeResyncRequest(message: string | Uint8Array): "resync" | null {
+  const text = typeof message === "string" ? message : Buffer.from(message).toString("utf8");
+  if (text.length > MAX_CLIENT_MESSAGE_BYTES) return null;
+  try {
+    const parsed: unknown = JSON.parse(text);
+    return typeof parsed === "object" &&
+      parsed !== null &&
+      (parsed as { type?: unknown }).type === DEVICE_FRAME_RESYNC_MESSAGE
+      ? "resync"
+      : null;
+  } catch {
+    return null;
+  }
+}
+
+export interface DeviceFrameSocketWriter {
+  readonly write: (bytes: Uint8Array) => void;
+  readonly sink: DeviceFrameSink;
+}
+
+/**
+ * Wrap a raw write function as a transport sink, accounting for bytes handed
+ * to the socket but not yet acknowledged as flushed. The transport reads that
+ * number to decide whether the client is keeping up.
+ */
+export function makeDeviceFrameSink(options: {
+  readonly send: (bytes: Uint8Array) => Promise<void> | void;
+  readonly isOpen: () => boolean;
+}): DeviceFrameSink {
+  let inFlightBytes = 0;
+  return {
+    send: (bytes) => {
+      inFlightBytes += bytes.byteLength;
+      const settle = () => {
+        inFlightBytes = Math.max(0, inFlightBytes - bytes.byteLength);
+      };
+      const result = options.send(bytes);
+      if (result instanceof Promise) result.then(settle, settle);
+      else settle();
+    },
+    bufferedAmount: () => inFlightBytes,
+    isOpen: options.isOpen,
+  };
+}
+
+/**
+ * Mount `GET /ws/device-frames?udid=...`. Requests without a device, or on a
+ * host with no device engine, are refused before the upgrade.
+ */
+export function makeDeviceFrameRouteLayer<R = never>(options: {
+  /**
+   * Same admission decision as the RPC upgrade. Passed in rather than imported
+   * so this module does not depend on the auth stack, and so tests can mount
+   * the route without one.
+   */
+  readonly authorizeUpgrade: (
+    request: HttpServerRequest.HttpServerRequest,
+  ) => Effect.Effect<boolean, never, R>;
+}) {
+  return Layer.effectDiscard(
+    Effect.gen(function* () {
+      const router = yield* HttpRouter.HttpRouter;
+      yield* router.add(
+        "GET",
+        DEVICE_FRAME_WS_PATH,
+        Effect.gen(function* () {
+          const request = yield* HttpServerRequest.HttpServerRequest;
+          const deviceService = yield* Effect.serviceOption(DeviceService);
+          if (deviceService._tag === "None" || !deviceService.value.supported) {
+            return HttpServerResponse.text("Device streaming is unavailable", { status: 404 });
+          }
+          const url = HttpServerRequest.toURL(request);
+          const udid = url?.searchParams.get(DEVICE_FRAME_WS_UDID_PARAM)?.trim();
+          if (!udid) {
+            return HttpServerResponse.text("Missing udid", { status: 400 });
+          }
+          if (!(yield* options.authorizeUpgrade(request))) {
+            return HttpServerResponse.text("Forbidden", { status: 403 });
+          }
+
+          const socket = yield* request.upgrade;
+          const writer = yield* socket.writer;
+          let open = true;
+          const sink = makeDeviceFrameSink({
+            send: (bytes) => Effect.runPromise(writer(bytes)).catch(() => undefined),
+            isOpen: () => open,
+          });
+          const unsubscribe = deviceService.value.manager.subscribeFrames(udid, sink);
+          yield* Effect.addFinalizer(() =>
+            Effect.sync(() => {
+              open = false;
+              unsubscribe();
+            }),
+          );
+          // The only thing a client may send is a resync request, when its
+          // decoder hits a sequence gap or an error. Handled here rather than
+          // as an RPC because it is a property of this stream, and because a
+          // frozen canvas should not depend on a second socket being healthy.
+          // Anything unrecognized is ignored: a stray message must not kill a
+          // stream.
+          yield* socket.run((message) => {
+            if (decodeResyncRequest(message) === null) return;
+            Effect.runFork(
+              Effect.promise(() =>
+                deviceService.value.manager.requestKeyframe(udid).catch(() => undefined),
+              ),
+            );
+          });
+          return HttpServerResponse.empty();
+        }).pipe(
+          Effect.catchCause((cause) =>
+            Effect.as(
+              Effect.logDebug("device frame socket closed", { cause: String(cause) }),
+              HttpServerResponse.empty(),
+            ),
+          ),
+        ),
+      );
+    }),
+  );
+}

piai-lab/Haros

apps/server/src/agentGateway/harnessPolicy.test.ts
@@ -94,4 +94,79 @@ describe("OmniMind harness policy", () => {
       assert.notInclude(text, "one exact omnimind_create_threads plan", provider);
     }
   });
+
+  it("teaches the device tools well enough for a plain prompt to work", () => {
+    const policy = renderOmniMindHarnessPolicy({ gatewayControlAvailable: true });
+
+    // When to reach for them at all: the demo needed "using your device_* tools"
+    // spelled out because the policy only triggered on the user naming a tool.
+    assert.include(policy, "run, test, check, demo, debug, or interact with an iOS app");
+    assert.include(policy, "whether or not the user names a tool");
+    assert.include(policy, "never drive the simulator with xcrun simctl");
+    // A rival agent-device skill on the host was read before the tools were
+    // tried; the browser guidance names its competitors, so this does too.
+    assert.include(policy, "do not load or use an agent-device");
+    assert.include(policy, "rather than reading skill files first");
+
+    // The workflow, so the agent does not have to guess an ordering.
+    assert.include(policy, "device_list first");
+    // Reusing a booted device rather than starting a second one: two live
+    // simulators compete for the pane and the user watches the wrong screen.
+    assert.include(policy, "already booted, use that one");
+    assert.include(policy, "device_install and device_launch");
+    assert.include(policy, "com.apple.Preferences");
+
+    // Expo/RN CLI paths boot the sim through Simulator.app, which foregrounds
+    // a window the user is not watching and leaves the OmniMind pane empty. A
+    // real demo also stalled for minutes on a dev server holding the shell.
+    assert.include(policy, "For Expo or React Native work");
+    assert.include(policy, "expo start --ios");
+    assert.include(policy, "npm run ios");
+    assert.include(policy, "opens Simulator.app");
+    assert.include(policy, "exp://127.0.0.1:8081");
+    assert.include(policy, "start it detached in the background");
+
+    // Interaction discipline: describe before tapping, verify after.
+    assert.include(policy, "device points from device_describe_ui, never screenshot pixels");
+    assert.include(policy, "again afterwards to confirm the screen changed");
+
+    // Semantic targeting is the headline: making the model do the coordinate
+    // arithmetic is where taps go wrong, so label targeting leads.
+    assert.include(policy, "Tap by label rather than by coordinates");
+    assert.include(policy, "device_tap {udid, label}");
+    assert.include(policy, "device_tap {udid, label, role}");
+    assert.include(policy, "only when nothing in the tree labels the target");
+
+    // Why a row-centre tap does nothing, for the cases still using coordinates.
+    assert.include(policy, "the row's frame centre is dead space");
+
+    // Scrolling is motor control the server owns. A demo agent swiped three
+    // times to reach Developer when one call should have done it.
+    assert.include(policy, "Never write a swipe loop");
+    assert.include(policy, "device_scroll_to_element {udid, label}");
+    assert.include(policy, "device_tap with a label already scrolls");
+    // device_swipe still has a job; this must not read as a blanket ban.
+    assert.include(policy, "gestures that are the point in themselves");
+
+    // Reading and verifying toggle state from the tree instead of pixels. The
+    // same run took a screenshot purely to work out whether the switch moved.
+    assert.include(policy, "A toggle reports its state in the node's value");
+    assert.include(policy, "Never take a screenshot to check state");
+    assert.include(policy, "device_screenshot is for showing the user a result");
+
+    // The traps that made the demo agent report success it never observed.
+    assert.include(policy, "an unchanged tree after a tap means the tap missed");
+    assert.include(policy, "not delivered to the simulator");
+    assert.include(policy, "Never report success you have not observed");
+    assert.include(policy, "device_open_url always requires explicit user approval");
+    assert.include(policy, "Airplane Mode");
+  });
+
+  it("withholds device guidance from sessions with no gateway control", () => {
+    const policy = renderOmniMindHarnessPolicy({ gatewayControlAvailable: false });
+
+    // Promising tools this session cannot reach would be a lie.
+    assert.notInclude(policy, "device_list");
+    assert.notInclude(policy, "device_describe_ui");
+  });
 });

apps/server/src/device/Services/DeviceService.ts
@@ -1,0 +1,18 @@
+import { ServiceMap } from "effect";
+
+import type { DeviceManager } from "../DeviceManager.ts";
+
+export interface DeviceServiceShape {
+  /**
+   * True only where a device backend can actually exist (macOS today). Off
+   * darwin the manager still answers, but every call reports
+   * `unsupported-platform`, and callers use this to hide the surface entirely
+   * rather than offering an agent eleven tools that cannot work.
+   */
+  readonly supported: boolean;
+  readonly manager: DeviceManager;
+}
+
+export class DeviceService extends ServiceMap.Service<DeviceService, DeviceServiceShape>()(
+  "omnimind/device/Services/DeviceService",
+) {}

apps/server/src/device/deviceFrameRoute.ts
@@ -1,0 +1,159 @@
+/**
+ * Device frame WebSocket route.
+ *
+ * Encoded video gets its own upgrade path rather than sharing the JSON RPC
+ * socket. Two reasons, both about not hurting the rest of the app:
+ *
+ * - Send-queue isolation. On one socket a video backlog sits ahead of RPC
+ *   responses in the same queue; on its own connection it cannot.
+ * - Compression. The RPC path negotiates per-message deflate; H.264 is already
+ *   compressed, so running it through zlib would burn CPU for nothing. This
+ *   path lands on the uncompressed WebSocket server (see
+ *   `upgradePathAllowsCompression`).
+ *
+ * Backpressure lives in `DeviceFrameTransport`; this module only adapts an
+ * Effect `Socket` into the transport's sink, tracking in-flight bytes itself
+ * because the Effect socket exposes no `bufferedAmount`.
+ *
+ * @module device/deviceFrameRoute
+ */
+import {
+  DEVICE_FRAME_RESYNC_MESSAGE,
+  DEVICE_FRAME_WS_PATH,
+  DEVICE_FRAME_WS_UDID_PARAM,
+} from "@omnimind/shared/deviceFrame";
+import { Effect, Layer } from "effect";
+import { HttpRouter, HttpServerRequest, HttpServerResponse } from "effect/unstable/http";
+
+import { DeviceService } from "./Services/DeviceService.ts";
+import type { DeviceFrameSink } from "./deviceFrameTransport.ts";
+
+/** A resync request is a few dozen bytes; anything larger is not one. */
+const MAX_CLIENT_MESSAGE_BYTES = 1_024;
+
+/**
+ * Parse a client message on the frame socket. Returns the request kind, or
+ * null for anything unrecognized, which the caller ignores rather than
+ * treating as a protocol error.
+ */
+export function decodeResyncRequest(message: string | Uint8Array): "resync" | null {
+  const text = typeof message === "string" ? message : Buffer.from(message).toString("utf8");
+  if (text.length > MAX_CLIENT_MESSAGE_BYTES) return null;
+  try {
+    const parsed: unknown = JSON.parse(text);
+    return typeof parsed === "object" &&
+      parsed !== null &&
+      (parsed as { type?: unknown }).type === DEVICE_FRAME_RESYNC_MESSAGE
+      ? "resync"
+      : null;
+  } catch {
+    return null;
+  }
+}
+
+export interface DeviceFrameSocketWriter {
+  readonly write: (bytes: Uint8Array) => void;
+  readonly sink: DeviceFrameSink;
+}
+
+/**
+ * Wrap a raw write function as a transport sink, accounting for bytes handed
+ * to the socket but not yet acknowledged as flushed. The transport reads that
+ * number to decide whether the client is keeping up.
+ */
+export function makeDeviceFrameSink(options: {
+  readonly send: (bytes: Uint8Array) => Promise<void> | void;
+  readonly isOpen: () => boolean;
+}): DeviceFrameSink {
+  let inFlightBytes = 0;
+  return {
+    send: (bytes) => {
+      inFlightBytes += bytes.byteLength;
+      const settle = () => {
+        inFlightBytes = Math.max(0, inFlightBytes - bytes.byteLength);
+      };
+      const result = options.send(bytes);
+      if (result instanceof Promise) result.then(settle, settle);
+      else settle();
+    },
+    bufferedAmount: () => inFlightBytes,
+    isOpen: options.isOpen,
+  };
+}
+
+/**
+ * Mount `GET /ws/device-frames?udid=...`. Requests without a device, or on a
+ * host with no device engine, are refused before the upgrade.
+ */
+export function makeDeviceFrameRouteLayer<R = never>(options: {
+  /**
+   * Same admission decision as the RPC upgrade. Passed in rather than imported
+   * so this module does not depend on the auth stack, and so tests can mount
+   * the route without one.
+   */
+  readonly authorizeUpgrade: (
+    request: HttpServerRequest.HttpServerRequest,
+  ) => Effect.Effect<boolean, never, R>;
+}) {
+  return Layer.effectDiscard(
+    Effect.gen(function* () {
+      const router = yield* HttpRouter.HttpRouter;
+      yield* router.add(
+        "GET",
+        DEVICE_FRAME_WS_PATH,
+        Effect.gen(function* () {
+          const request = yield* HttpServerRequest.HttpServerRequest;
+          const deviceService = yield* Effect.serviceOption(DeviceService);
+          if (deviceService._tag === "None" || !deviceService.value.supported) {
+            return HttpServerResponse.text("Device streaming is unavailable", { status: 404 });
+          }
+          const url = HttpServerRequest.toURL(request);
+          const udid = url?.searchParams.get(DEVICE_FRAME_WS_UDID_PARAM)?.trim();
+          if (!udid) {
+            return HttpServerResponse.text("Missing udid", { status: 400 });
+          }
+          if (!(yield* options.authorizeUpgrade(request))) {
+            return HttpServerResponse.text("Forbidden", { status: 403 });
+          }
+
+          const socket = yield* request.upgrade;
+          const writer = yield* socket.writer;
+          let open = true;
+          const sink = makeDeviceFrameSink({
+            send: (bytes) => Effect.runPromise(writer(bytes)).catch(() => undefined),
+            isOpen: () => open,
+          });
+          const unsubscribe = deviceService.value.manager.subscribeFrames(udid, sink);
+          yield* Effect.addFinalizer(() =>
+            Effect.sync(() => {
+              open = false;
+              unsubscribe();
+            }),
+          );
+          // The only thing a client may send is a resync request, when its
+          // decoder hits a sequence gap or an error. Handled here rather than
+          // as an RPC because it is a property of this stream, and because a
+          // frozen canvas should not depend on a second socket being healthy.
+          // Anything unrecognized is ignored: a stray message must not kill a
+          // stream.
+          yield* socket.run((message) => {
+            if (decodeResyncRequest(message) === null) return;
+            Effect.runFork(
+              Effect.promise(() =>
+                deviceService.value.manager.requestKeyframe(udid).catch(() => undefined),
+              ),
+            );
+          });
+          return HttpServerResponse.empty();
+        }).pipe(
+          Effect.catchCause((cause) =>
+            Effect.as(
+              Effect.logDebug("device frame socket closed", { cause: String(cause) }),
+              HttpServerResponse.empty(),
+            ),
+          ),
+        ),
+      );
+    }),
+  );
+}